dbda115f7033f535970b527c4cc3c4686f848b0c
bhaskarpandey1234 <bhaskarindiapandey@gmail.com>
2023-09-28 15:47:57 +0530

    first commit

---
+++ src/main/java/com/springboot/web/Springbootp2Application.java
@@ -0,0 +1,13 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootp2Application.class, args);
+	}
+
+}
